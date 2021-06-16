b12469a91e690cd17cb19a3b2a928ff95cfc1dfc
mateigraura <grauramatei@gmail.com>
2021-06-17 01:25:30 +0300

    rand sample test

--- filters/bloom_test.go
+++ filters/bloom_test.go
@@ -1,10 +1,23 @@
 package filters
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func getRandSample(n int) [][]byte {
+	var topics [][]byte
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < n; i++ {
+		el := make([]byte, 30)
+		rand.Read(el)
+		topics = append(topics, el)
+	}
+	return topics
+}
 
 func TestBloom_IsInSetFromStrings(t *testing.T) {
 	t.Parallel()
@@ -38,5 +51,24 @@ func TestBloom_IsInSetFromHexStrings(t *testing.T) {
 
 	require.True(t, bloom.IsInSet(d1))
 	require.True(t, bloom.IsInSet(d2))
-	require.False(t, bloom.IsInSet([]byte("3135")))
+	require.False(t, bloom.IsInSet([]byte("3134")))
+}
+
+func TestBloom_IsInSetRandSample(t *testing.T) {
+	t.Parallel()
+	rand.Seed(time.Now().UnixNano())
+
+	n := 100_000
+
+	topics := getRandSample(n)
+
+	bloom := NewBloom(uint(n))
+	_ = bloom.SetMany(topics)
+
+	for _, topic := range topics {
+		ok := bloom.IsInSet(topic)
+		if !ok {
+			t.Errorf("%s should be in set", string(topic))
+		}
+	}
 }
