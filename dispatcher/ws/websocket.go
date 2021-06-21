071f475f79cb6ca9cd2083a5291c687ad2c42b3b
mateigraura <grauramatei@gmail.com>
2021-06-21 12:18:55 +0300

    small refactor for ws client

--- dispatcher/ws/websocket.go
+++ dispatcher/ws/websocket.go
@@ -40,7 +40,7 @@ type websocketDispatcher struct {
 	hub  dispatcher.Hub
 }
 
-func NewWebsocketDispatcher(
+func newWebsocketDispatcher(
 	conn *websocket.Conn,
 	hub dispatcher.Hub,
 ) *websocketDispatcher {
@@ -52,24 +52,26 @@ func NewWebsocketDispatcher(
 	}
 }
 
-// Serve is the entry point used by a http server to serve the ws upgrader
+// Serve is the entry point used by a http server to serve the websocket upgrader
 func Serve(hub dispatcher.Hub, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	wsDispatcher := NewWebsocketDispatcher(conn, hub)
+	wsDispatcher := newWebsocketDispatcher(conn, hub)
 	wsDispatcher.hub.RegisterChan() <- wsDispatcher
 
 	go wsDispatcher.writePump()
 	go wsDispatcher.readPump()
 }
 
+// GetID returns the id corresponding to this dispatcher instance
 func (wd *websocketDispatcher) GetID() uuid.UUID {
 	return wd.id
 }
 
+// PushEvents receives an events slice and processes it before pushing to socket
 func (wd *websocketDispatcher) PushEvents(events []data.Event) {
 	eventBytes, err := json.Marshal(events)
 	if err != nil {
@@ -79,7 +81,7 @@ func (wd *websocketDispatcher) PushEvents(events []data.Event) {
 	wd.send <- eventBytes
 }
 
-// writePump listens on the send channel and pushes data on the ws stream
+// writePump listens on the send channel and pushes data on the socket stream
 func (wd *websocketDispatcher) writePump() {
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
@@ -89,62 +91,74 @@ func (wd *websocketDispatcher) writePump() {
 		}
 	}()
 
+	nextWriterWrap := func(msgType int, data []byte) error {
+		writer, err := wd.conn.NextWriter(msgType)
+		if err != nil {
+			return err
+		}
+		_, err = writer.Write(data)
+		if err != nil {
+			return err
+		}
+		return writer.Close()
+	}
+
 	for {
 		select {
-		case msg, ok := <-wd.send:
-			_ = wd.conn.SetWriteDeadline(time.Now().Add(writeWait))
+		case message, ok := <-wd.send:
+			if err := wd.setSocketWriteLimits(); err != nil {
+				log.Println("channel: failed to set socket write limits", err.Error())
+				return
+			}
+
 			if !ok {
-				_ = wd.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
+				if err := wd.conn.WriteMessage(websocket.CloseMessage, []byte{}); err != nil {
+					log.Println("failed to write close message", err.Error())
+					return
+				}
 			}
 
-			w, err := wd.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			_, _ = w.Write(msg)
-
-			if err = w.Close(); err != nil {
-				log.Println(err)
+			if err := nextWriterWrap(websocket.TextMessage, message); err != nil {
+				log.Println("failed to write text message", err.Error())
 				return
 			}
 		case <-ticker.C:
-			_ = wd.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := wd.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				log.Println(err)
+			if err := wd.setSocketWriteLimits(); err != nil {
+				log.Println("ticker: failed to set socket write limits")
+			}
+			if err := wd.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Println("ticker: failed to write ping message", err.Error())
 				return
 			}
 		}
 	}
 }
 
-// readPump listens for incoming events and reads the content
+// readPump listens for incoming events and reads the content from the socket stream
 func (wd *websocketDispatcher) readPump() {
 	defer func() {
 		wd.hub.UnregisterChan() <- wd
 		if err := wd.conn.Close(); err != nil {
 			log.Println(err)
 		}
+		close(wd.send)
 	}()
 
-	wd.conn.SetReadLimit(maxMsgSize)
-	_ = wd.conn.SetReadDeadline(time.Now().Add(pongWait))
-	wd.conn.SetPongHandler(func(string) error {
-		_ = wd.conn.SetReadDeadline(time.Now().Add(pongWait))
-		return nil
-	})
+	if err := wd.setSocketReadLimits(); err != nil {
+		log.Println("failed to set socket read limits", err.Error())
+
+	}
 
 	for {
-		_, msg, err := wd.conn.ReadMessage()
-		if err != nil {
+		_, msg, innerErr := wd.conn.ReadMessage()
+		if innerErr != nil {
+			log.Println("failure reading socket", innerErr.Error())
 			if websocket.IsUnexpectedCloseError(
-				err,
+				innerErr,
 				websocket.CloseGoingAway,
 				websocket.CloseAbnormalClosure,
 			) {
-				log.Println(err)
+				log.Println("unexpected socket close error", innerErr.Error())
 			}
 			break
 		}
@@ -152,4 +166,22 @@ func (wd *websocketDispatcher) readPump() {
 		// TODO: get msg from stream -> reject for bad type, else push to hub
 		_ = bytes.TrimSpace(bytes.Replace(msg, newline, space, -1))
 	}
+}
+
+func (wd *websocketDispatcher) setSocketWriteLimits() error {
+	if err := wd.conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (wd *websocketDispatcher) setSocketReadLimits() error {
+	wd.conn.SetReadLimit(maxMsgSize)
+	if err := wd.conn.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		return err
+	}
+	wd.conn.SetPongHandler(func(string) error {
+		return wd.conn.SetReadDeadline(time.Now().Add(pongWait))
+	})
+	return nil
 }
