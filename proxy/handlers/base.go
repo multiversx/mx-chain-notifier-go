13bad4cc8ed27fd3db4fa4922b4a1897f59e7004
mateigraura <grauramatei@gmail.com>
2021-07-22 13:22:21 +0300

    change groupHandler placeholder

--- proxy/handlers/base.go
+++ proxy/handlers/base.go
@@ -2,6 +2,12 @@ package handlers
 
 import "github.com/gin-gonic/gin"
 
+type EndpointGroupHandler struct {
+	Root             string
+	Middleware       []gin.HandlerFunc
+	EndpointHandlers []EndpointHandler
+}
+
 type EndpointHandler struct {
 	Path        string
 	Method      string
@@ -9,23 +15,23 @@ type EndpointHandler struct {
 }
 
 type groupHandler struct {
-	endpointHandlersMap map[string][]EndpointHandler
+	endpointHandlersMap map[string]EndpointGroupHandler
 }
 
 // NewGroupHandler creates an instance of a groupHandler
 func NewGroupHandler() *groupHandler {
 	return &groupHandler{
-		endpointHandlersMap: make(map[string][]EndpointHandler),
+		endpointHandlersMap: make(map[string]EndpointGroupHandler),
 	}
 }
 
 // RegisterEndpoints registers the endpoints groups and the corresponding handlers
 // It should be called after all the handlers have been defined
 func (g *groupHandler) RegisterEndpoints(r *gin.Engine) {
-	for route, handlersGroup := range g.endpointHandlersMap {
-		routerGroup := r.Group(route)
+	for groupRoot, handlersGroup := range g.endpointHandlersMap {
+		routerGroup := r.Group(groupRoot, handlersGroup.Middleware...)
 		{
-			for _, h := range handlersGroup {
+			for _, h := range handlersGroup.EndpointHandlers {
 				routerGroup.Handle(h.Method, h.Path, h.HandlerFunc)
 			}
 		}
@@ -35,8 +41,8 @@ func (g *groupHandler) RegisterEndpoints(r *gin.Engine) {
 // AddEndpointHandlers inserts a list of endpoint handlers to the map
 // The key of the endpointHandlersMap is the base path of the group
 // The method is not thread-safe and does not validate inputs
-func (g *groupHandler) AddEndpointHandlers(route string, handlers []EndpointHandler) {
-	g.endpointHandlersMap[route] = handlers
+func (g *groupHandler) AddEndpointHandlers(route string, endpointHandler EndpointGroupHandler) {
+	g.endpointHandlersMap[endpointHandler.Root] = endpointHandler
 }
 
 type apiResponse struct {
