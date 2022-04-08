e976b815d077106de9a43a7b41e6cd8223aa054b
ssd04 <dariussuirab@gmail.com>
2022-04-08 08:30:22 +0300

    update rabbit client mock with updated interface

--- mocks/rabbitMqClientStub.go
+++ mocks/rabbitMqClientStub.go
@@ -4,7 +4,12 @@ import "github.com/streadway/amqp"
 
 // RabbitClientStub -
 type RabbitClientStub struct {
-	PublishCalled func(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error
+	PublishCalled       func(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error
+	ConnErrChanCalled   func() chan *amqp.Error
+	CloseErrChanCalled  func() chan *amqp.Error
+	ReconnectCalled     func()
+	ReopenChannelCalled func()
+	CloseCalled         func()
 }
 
 // Publish -
@@ -13,6 +18,43 @@ func (rc *RabbitClientStub) Publish(exchange, key string, mandatory, immediate b
 		return rc.PublishCalled(exchange, key, mandatory, immediate, msg)
 	}
 	return nil
+}
+
+// ConnErrChan -
+func (rc *RabbitClientStub) ConnErrChan() chan *amqp.Error {
+	if rc.ConnErrChanCalled != nil {
+		return rc.ConnErrChanCalled()
+	}
+	return nil
+}
+
+// CloseErrChan -
+func (rc *RabbitClientStub) CloseErrChan() chan *amqp.Error {
+	if rc.CloseErrChanCalled != nil {
+		return rc.CloseErrChanCalled()
+	}
+	return nil
+}
+
+// Reconnect -
+func (rc *RabbitClientStub) Reconnect() {
+	if rc.ReconnectCalled != nil {
+		rc.ReconnectCalled()
+	}
+}
+
+// ReopenChannel -
+func (rc *RabbitClientStub) ReopenChannel() {
+	if rc.ReopenChannelCalled != nil {
+		rc.ReopenChannelCalled()
+	}
+}
+
+// Close -
+func (rc *RabbitClientStub) Close() {
+	if rc.CloseCalled != nil {
+		rc.CloseCalled()
+	}
 }
 
 // IsInterfaceNil -
