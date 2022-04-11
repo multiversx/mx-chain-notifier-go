a3ae2d31031f2caf933fb808cee3480c8578ef6b
ssd04 <dariussuirab@gmail.com>
2022-04-11 11:22:14 +0300

    add time sleeps in websocket tests

--- integrationTests/websocket/testNotifierWithWebsockets_test.go
+++ integrationTests/websocket/testNotifierWithWebsockets_test.go
@@ -1,7 +1,10 @@
 package websocket
 
 import (
+	"encoding/json"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/ElrondNetwork/notifier-go/common"
 	"github.com/ElrondNetwork/notifier-go/data"
@@ -46,6 +49,8 @@ func TestNotifierWithWebsockets_PushEvents(t *testing.T) {
 	resp := webServer.PushEventsRequest(blockEvents)
 	require.NotNil(t, resp)
 
+	time.Sleep(time.Second)
+
 	reply, err := ws.ReceiveEvents()
 	require.Nil(t, err)
 
@@ -83,6 +88,8 @@ func TestNotifierWithWebsockets_RevertEvents(t *testing.T) {
 	resp := webServer.RevertEventsRequest(blockEvents)
 	require.NotNil(t, resp)
 
+	time.Sleep(time.Second)
+
 	reply, err := ws.ReceiveRevertBlock()
 	require.Nil(t, err)
 
@@ -119,104 +126,106 @@ func TestNotifierWithWebsockets_FinalizedEvents(t *testing.T) {
 	resp := webServer.FinalizedEventsRequest(blockEvents)
 	require.NotNil(t, resp)
 
+	time.Sleep(time.Second)
+
 	reply, err := ws.ReceiveFinalized()
 	require.Nil(t, err)
 
 	assert.Equal(t, blockEvents, reply)
 }
 
-// func TestNotifierWithWebsockets_AllEvents(t *testing.T) {
-// 	cfg := integrationTests.GetDefaultConfigs()
-// 	cfg.ConnectorApi.CheckDuplicates = true
-// 	notifier, err := integrationTests.NewTestNotifierWithWS(cfg)
-// 	require.Nil(t, err)
+func TestNotifierWithWebsockets_AllEvents(t *testing.T) {
+	cfg := integrationTests.GetDefaultConfigs()
+	cfg.ConnectorApi.CheckDuplicates = true
+	notifier, err := integrationTests.NewTestNotifierWithWS(cfg)
+	require.Nil(t, err)
 
-// 	webServer := integrationTests.NewTestWebServer(notifier.Facade, common.MessageQueueAPIType)
+	webServer := integrationTests.NewTestWebServer(notifier.Facade, common.MessageQueueAPIType)
 
-// 	notifier.Publisher.Run()
-// 	defer notifier.Publisher.Close()
+	notifier.Publisher.Run()
+	defer notifier.Publisher.Close()
 
-// 	ws, err := integrationTests.NewWSClient(notifier.WSHandler)
-// 	require.Nil(t, err)
-// 	defer ws.Close()
+	ws, err := integrationTests.NewWSClient(notifier.WSHandler)
+	require.Nil(t, err)
+	defer ws.Close()
 
-// 	subscribeEvent := &data.SubscribeEvent{
-// 		SubscriptionEntries: []data.SubscriptionEntry{
-// 			{
-// 				EventType: "all_events",
-// 			},
-// 			{
-// 				EventType: "revert_events",
-// 			},
-// 			{
-// 				EventType: "finalized_events",
-// 			},
-// 		},
-// 	}
+	subscribeEvent := &data.SubscribeEvent{
+		SubscriptionEntries: []data.SubscriptionEntry{
+			{
+				EventType: "all_events",
+			},
+			{
+				EventType: "revert_events",
+			},
+			{
+				EventType: "finalized_events",
+			},
+		},
+	}
 
-// 	err = ws.SendSubscribeMessage(subscribeEvent)
-// 	require.Nil(t, err)
+	err = ws.SendSubscribeMessage(subscribeEvent)
+	require.Nil(t, err)
 
-// 	revertBlock := &data.RevertBlock{
-// 		Hash:  "hash1",
-// 		Nonce: 1,
-// 	}
-// 	finalizedBlock := &data.FinalizedBlock{
-// 		Hash: "hash1",
-// 	}
+	revertBlock := &data.RevertBlock{
+		Hash:  "hash1",
+		Nonce: 1,
+	}
+	finalizedBlock := &data.FinalizedBlock{
+		Hash: "hash1",
+	}
 
-// 	events := []data.Event{
-// 		{
-// 			Address: "addr1",
-// 		},
-// 	}
-// 	blockEvents := &data.BlockEvents{
-// 		Hash:   "hash1",
-// 		Events: events,
-// 	}
+	events := []data.Event{
+		{
+			Address: "addr1",
+		},
+	}
+	blockEvents := &data.BlockEvents{
+		Hash:   "hash1",
+		Events: events,
+	}
 
-// 	wg := &sync.WaitGroup{}
-// 	wg.Add(3)
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
 
-// 	go func(wg *sync.WaitGroup) {
-// 		for i := 0; i < 3; i++ {
-// 			m, err := ws.ReadMessage()
-// 			require.Nil(t, err)
+	go func(wg *sync.WaitGroup) {
+		for i := 0; i < 3; i++ {
+			m, err := ws.ReadMessage()
+			require.Nil(t, err)
 
-// 			var reply data.WSEvent
-// 			err = json.Unmarshal(m, &reply)
-// 			require.Nil(t, err)
+			var reply data.WSEvent
+			err = json.Unmarshal(m, &reply)
+			require.Nil(t, err)
 
-// 			switch reply.Type {
-// 			case common.PushBlockEvents:
-// 				var event []data.Event
-// 				_ = json.Unmarshal(reply.Data, &event)
-// 				assert.Equal(t, events, event)
-// 				wg.Done()
-// 			case common.RevertBlockEvents:
-// 				var event *data.RevertBlock
-// 				_ = json.Unmarshal(reply.Data, &event)
-// 				assert.Equal(t, revertBlock, event)
-// 				wg.Done()
-// 			case common.FinalizedBlockEvents:
-// 				var event *data.FinalizedBlock
-// 				_ = json.Unmarshal(reply.Data, &event)
-// 				assert.Equal(t, finalizedBlock, event)
-// 				wg.Done()
-// 			default:
-// 				t.Errorf("invalid message type")
-// 			}
-// 		}
-// 	}(wg)
+			switch reply.Type {
+			case common.PushBlockEvents:
+				var event []data.Event
+				_ = json.Unmarshal(reply.Data, &event)
+				assert.Equal(t, events, event)
+				wg.Done()
+			case common.RevertBlockEvents:
+				var event *data.RevertBlock
+				_ = json.Unmarshal(reply.Data, &event)
+				assert.Equal(t, revertBlock, event)
+				wg.Done()
+			case common.FinalizedBlockEvents:
+				var event *data.FinalizedBlock
+				_ = json.Unmarshal(reply.Data, &event)
+				assert.Equal(t, finalizedBlock, event)
+				wg.Done()
+			default:
+				t.Errorf("invalid message type")
+			}
+		}
+	}(wg)
 
-// 	time.Sleep(time.Second)
-// 	go webServer.PushEventsRequest(blockEvents)
-// 	time.Sleep(time.Second)
-// 	go webServer.FinalizedEventsRequest(finalizedBlock)
-// 	time.Sleep(time.Second)
-// 	go webServer.RevertEventsRequest(revertBlock)
+	time.Sleep(time.Second)
+	go webServer.PushEventsRequest(blockEvents)
+	time.Sleep(time.Second)
+	go webServer.FinalizedEventsRequest(finalizedBlock)
+	time.Sleep(time.Second)
+	go webServer.RevertEventsRequest(revertBlock)
 
-// 	wg.Wait()
+	wg.Wait()
 
-// 	assert.Equal(t, 3, len(notifier.RedisClient.GetEntries()))
-// }
+	assert.Equal(t, 3, len(notifier.RedisClient.GetEntries()))
+}
