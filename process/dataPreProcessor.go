892ec7da1c080fdffab82e228f7e3952c63ce855
ssd04 <dariussuirab@gmail.com>
2023-06-12 11:52:47 +0300

    fix header hash reference

--- process/dataPreProcessor.go
+++ process/dataPreProcessor.go
@@ -141,7 +141,7 @@ func (d *eventsDataPreProcessor) RevertIndexedBlock(blockData *outport.BlockData
 	}
 
 	revertData := &data.RevertBlock{
-		Hash:  hex.EncodeToString(header.GetRootHash()),
+		Hash:  hex.EncodeToString(blockData.GetHeaderHash()),
 		Nonce: header.GetNonce(),
 		Round: header.GetRound(),
 		Epoch: header.GetEpoch(),
