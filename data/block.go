28e4b027484f7095266d4d6500624989577ea1b8
ssd04 <dariussuirab@gmail.com>
2026-01-23 18:28:36 +0200

    fix execution result timestamp

--- data/block.go
+++ data/block.go
@@ -33,6 +33,7 @@ type InterceptorBlockData struct {
 	LogEvents                []Event
 	StateAccessesPerAccounts map[string]*stateChange.StateAccesses
 	Nonce                    uint64
+	TimeStampMs              uint64
 }
 
 // ArgsSaveBlockData holds the block data that will be received on push events
